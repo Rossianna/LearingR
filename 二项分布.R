00ea1dfb62aeb9c697124d599e8b6d811e64a9f2
X <54944183+Rossianna@users.noreply.github.com>
2021-04-19 16:55:07 +0800

    Create 二项分布.R

---
+++ 二项分布.R
@@ -0,0 +1,4 @@
+#dbinom函数，can be used to get binomial probability mass function(概率质量函数)
+dbinom(x,m,n)
+# x:x successes, m:m trials, n:probability of success in each trial,如dbinom(x,40,0.1)表示在40次试验中，单次成功的概率为0.1，则x次(x=0:40)成功的概率分别为多少
+
