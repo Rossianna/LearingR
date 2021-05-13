c212436cbd21347b04b0a9ad8c313be265973fe1
X <54944183+Rossianna@users.noreply.github.com>
2021-05-14 00:04:38 +0800

    Create R语言函数操作.R

---
+++ R语言函数操作.R
@@ -0,0 +1,19 @@
+# R语言用function()定义函数,括号中为参数，前面好像都需要有加号
+# factorial求阶乘
+y=function(n){
++ out = 1 - sum((-1)^(0:n)/factorial(0:n))
++ out
+}
+
+# R语言的for循环,对于单个语句操作可以不加{}，对于多个语句还是需要{}，这点和Java还有C语言一样
+for(n in 1:6) # 相当于其他编程语言中 for(n=1; n<6; n++)
+
+# R语言的sample抽样函数
+x=1:10
+sample(x, size=5, replace=TRUE) # 表示从1~10中选择5个数(样本大小为5)，replace=TRUE表示是有放回的抽样，如果不写replace参数，则默认是无放回的抽样
+
+# R语言中也使用==作为布尔类型的判断，判断结果为TRUE 或 FALSE
+x = sample(1:5,5,replace=F)
+x == (1:5)
+sum(x == (1:5)) 
+# 以上是求x=1~5之间的值的个数
